98bd92250c0564ff5957cd80256f98f31d61a0ed
Spares <macdonald.glen.a@gmail.com>
2023-03-01 22:04:48 +1100

    Checks number of brackets and operators

--- src/bin/check.rs
+++ src/bin/check.rs
@@ -1,3 +1,22 @@
-pub fn check(input_string: String) -> String {
+pub fn check(input_string: &str) -> bool {
+    let n_left_brackets: usize = input_string.chars().filter(|c| *c == '[').count();
+    let n_right_brackets: usize = input_string.chars().filter(|c| *c == ']').count();
+    
+    let mut n_operators: usize = input_string.chars().filter(|c| *c == '&').count();
+    n_operators += input_string.chars().filter(|c| *c == '|').count();
+    n_operators += input_string.matches("=>").count();
 
+    if n_left_brackets != n_right_brackets {
+        return false;
+    } 
+
+    if n_operators != n_left_brackets {
+        return false;
+    }
+
+    return true;
+}
+
+pub fn main() {
+    
 }
